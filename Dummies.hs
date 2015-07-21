4a7b3693626542a68f852f205a6c5027eab3654b
ImCoding4Fun <giudicealdo@gmail.com>
2015-07-21 18:53:52 +0200

    Set of dummy utility
    Here is my first dummy utilities -almost random stuff- I created

---
+++ Dummies.hs
@@ -0,0 +1,25 @@
+import Data.Char (toLower,toUpper)
+import Data.List (takeWhile, dropWhile)
+
+factorial n = product [1..n]
+
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs_up_to_n n = take n (fibs)
+
+toUpperString string = map toUpper string
+
+{-
+	Count the number of occurrences of a specific character in a string
+-}
+countOfElem elem list = length $ filter (\x -> x == elem) list
+
+{-  
+	Input: A single string or two words separated by a single space (otherwise it does not work...)
+	Returns a camel cased string 
+	I know this sucks; see here https://gist.github.com/ruthenium/3715696 how it should be written 
+-}
+toCamelCase string = if countOfElem ' ' string == 1 && length(words string) >= 2 then
+					 toLower(head string) : tail (map toLower (takeWhile (/=' ') string)) ++ 
+					 toUpper((dropWhile (/=' ') string)!!1) : tail(tail (map toLower (dropWhile (/=' ') string)))
+					 else
+					 "I'm not so smart yet"
