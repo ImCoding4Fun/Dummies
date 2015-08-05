fdd5090f0bb437a161b585ab230e1bfe6ae5d01c
ImCoding4Fun <giudicealdo@gmail.com>
2015-08-05 19:15:28 +0200

    Yet another dummy.
    No description.

--- Dummies.hs
+++ Dummies.hs
@@ -1,37 +1,48 @@
+{-
+    Author: A.F.G.
+    Content: Haskell first dummies. Training purpose only. To be continued.
+    Last update: 08-05-2015.
+-}
+
 import Data.Char (toLower,toUpper)
 import Data.List (takeWhile, dropWhile)
 
+-- Factorial of a number
 factorial n = product [1..n]
 
+-- Fibonacci sequence
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-fibs_up_to_n n = take n (fibs)
 
+-- Fibonacci sequence (up to n)
+fibs_up_to_n n = take n (fibs)
+--To upper of a string
 toUpperString string = map toUpper string
 
-{-
-	Count the number of occurrences of a specific character in a string
--}
+-- Count the number of occurrences of a specific character in a string
 countOfElem elem list = length $ filter (\x -> x == elem) list
 
 {-  
-	Input: A single string or two words separated by a single space (otherwise it does not work...)
-	Returns a camel cased string 
-	I know this sucks; see here https://gist.github.com/ruthenium/3715696 how it should be written 
+Input: A single string or two words separated by a single space (otherwise it does not work...)
+Returns a camel cased string 
+I know this sucks; see here https://gist.github.com/ruthenium/3715696 how it should be written 
 -}
 toCamelCase string = if countOfElem ' ' string == 1 && length(words string) >= 2 then
 					 toLower(head string) : tail (map toLower (takeWhile (/=' ') string)) ++ 
 					 toUpper((dropWhile (/=' ') string)!!1) : tail(tail (map toLower (dropWhile (/=' ') string)))
 					 else
 					 "I'm not so smart yet"
-{-
-Prime numbers
--}
+-- Prime numbers
 primes :: [Integer]
 primes = sieve [2..]
   where
     sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0]
 
-{-
-nth Prime number
--}
-nthPrime n = primes !!(n-1)
+-- nth Prime number
+nthPrime n = 
+         if n <= 1 then error("Invalid input parameter. (Should be greater than zero.)")
+         else primes !!(n-1)
+
+-- First n primes
+first_nthPrimes n =
+    if n > 2000 then error("This would take far too long...")
+    else take n (primes)
