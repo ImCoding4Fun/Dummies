609ad413a3fe5935a1947d5ab7ebf88806a33ff0
ImCoding4Fun <giudicealdo@gmail.com>
2015-08-24 18:27:57 +0200

    more fun
    fun

--- Dummies.hs
+++ Dummies.hs
@@ -5,11 +5,41 @@
 -}
 
 import Data.Char (toLower,toUpper)
-import Data.List (takeWhile, dropWhile)
+import Data.List (takeWhile, dropWhile,tails)
+{-
+
+2^3
+8
+
+flip (^) 2 3
+9
+
+3^2
+9
+
+flip (^) 3 2
+8
+
+[ x |x<- [1..100], odd x]
+
+filter (odd) [1..100]
+filter (\x -> x `mod` 2 == 0) [1..10]
+
+map (*2) [1..10]
+
+map (\x -> if odd x then x*2 else x*3) [1..10]
+
+length $ filter (\ x -> even x && x > 10) [1..20]
+
+-}
 
 -- Hello world
 hello_world :: IO ()
 hello_world = putStrLn "Hello, World!"
+
+--Back to the basics! Tables
+--table :: Int -> Integer
+table n = zipWith (*) [1..10] (repeat n)
 
 --Simple IO program
 input_user_info :: IO String
@@ -24,6 +54,10 @@ input_user_info = do
 -- Factorial of a number
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+
+-- Funnier factorial
+fact :: (Num b, Enum b) => b -> b
+fact n = foldl (\acc x -> acc * x) 1 [2..n]
 
 -- Fibonacci sequence
 fibonacci :: [Integer]
@@ -69,3 +103,36 @@ first_nthPrimes n =
     if n > 2000 then error("This would take far too long...")
     else take n (primes)
     
+--List of all the powers of n where the result is less than m 
+powsOf_N_UpTo_M n m = takeWhile (< m ) $ map (^n) [1..]
+
+{-
+Quicksort
+-}
+quicksort :: (Ord a) => [a] -> [a]    
+quicksort [] = []    
+quicksort (x:xs) =     
+    let smallerSorted = quicksort (filter (<=x) xs)  
+        biggerSorted = quicksort (filter (>x) xs)   
+    in  smallerSorted ++ [x] ++ biggerSorted  
+    
+    
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]  
+chain n  
+    | even n =  n:chain (n `div` 2)  
+    | odd n  =  n:chain (n*3 + 1) 
+    
+{-
+map (negate . sum . tail) [[1..5],[3..6],[1..7]]  
+
+[ (replicate 4) x | x<- [1..10] ]
+-}
+
+{-
+ Combinations
+-}
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] 
+                                  , x <- combinations (n-1) $ drop (i+1) xs ]
