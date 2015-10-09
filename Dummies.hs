efd264c66860ed5b0cb9af4f5ece101a7d401b21
ImCoding4Fun <giudicealdo@gmail.com>
2015-10-09 17:48:10 +0200

    m.c.
    m.c.

--- Dummies.hs
+++ Dummies.hs
@@ -128,3 +128,16 @@ solveRPN = head . foldl foldingFunction [] . words
             foldingFunction (x:xs) "ln" = log x:xs
             foldingFunction xs "sum" = [sum xs]
             foldingFunction xs numberString = read numberString:xs
+
+{-
+Modad = computation builder
+-}
+doubleOdds = [1..10] >>= (\x -> if odd x then [x*2] else [])
+
+example :: [(Int, Int, Int)]
+example = do
+  a <- [1,2]
+  b <- [10,20]
+  c <- [100,200]
+  return (a,b,c)
+-- [(1,10,100),(1,10,200),(1,20,100),(1,20,200),(2,10,100),(2,10,200),(2,20,100),(2,20,200)]
